c90c13bd2f76da5b9be0037553bec0459b82ed51
yongqianme <93888+yongqianme@users.noreply.github.com>
2024-04-11 12:01:18 -0400

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,10 @@
+import { defineNuxtConfig } from '@nuxt/bridge'
+
 export default defineNuxtConfig({
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false
+    }
+  },
   extends: '@nuxt-themes/docus'
 })
